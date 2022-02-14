247453052dad597d81261ec108219262400ee748
21mstanton <mstanton887@gmail.com>
2022-02-14 00:52:59 -0700

    5.1 reading practice

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+//this is part of the arrayList 5.1 reading practice
+ArrayList storFifty = new ArrayList(50);
+Account RobsAccount = new Account("Rob");
+//adding to an array list
+storFifty.Add(RobsAccount);
+//accessing items in an arrayList
+Account a = (Account) storFifty[0]; 
+a.PayInFunds(50);
+//removing an item
+storFifty.Remove(RobsAccount);
+//finding the size of the array
+if(storFifty.Count == 0)
+{
+    Console.WriteLine("no money here");
+}
